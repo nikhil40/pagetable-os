6fb7cec034c904a91dcd0c1f669d5a72da4651e6
Nikhil singh <49245217+nikhil40@users.noreply.github.com>
2019-04-16 22:49:55 +0530

    Update pagetable.c

--- pagetable.c
+++ pagetable.c
@@ -4,4 +4,11 @@ int main()
 int i,j,n,a[5120],frame[10],no,k,avail,count=0,pagesize[2000],page;
             printf("\n ENTER THE NUMBER OF PAGES:\n");
 scanf("%d",&n);
-           
+   printf("\n ENTER THE PAGE NUMBER :\n");
+            for(i=1;i<=n;i++)
+            scanf("%d",&a[i]);
+            printf("\n ENTER THE NUMBER OF FRAMES :");
+            scanf("%d",&no);
+            printf("\n ENTER THE PAGE SIZE :");
+            for(i=1;i<=n;i++)
+            scanf("%d",&page);         
